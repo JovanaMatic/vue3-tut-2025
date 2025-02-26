e9d21e5d4efb04be5172ec03a8562a37aabd8fa5
”Jovana <jovana.stankovic85@yahoo.com>
2025-02-26 23:18:54 +0100

    Add nav bar routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import JobsView from '../views/JobsView.vue';
+import AddJobView from '../views/AddJobView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,16 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: '/jobs',
+      name: 'jobs',
+      component: JobsView,
+    },
+    {
+      path: '/jobs/add',
+      name: 'add-job',
+      component: AddJobView,
     }
   ]
 })
---
+++ src/views/AddJobView.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Add job</h1>
+</template>
