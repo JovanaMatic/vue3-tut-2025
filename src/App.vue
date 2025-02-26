dc206c52d630c7c67e63f8725abddc484b447bb6
”Jovana <jovana.stankovic85@yahoo.com>
2025-02-26 18:23:26 +0100

    Set up vue router

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <script setup>
-import { ref } from 'vue'
 import Navbar from '@/components/Navbar.vue'
-import Hero from '@/components/Hero.vue'
-import HomeCards from '@/components/HomeCards.vue'
-import JobListings from '@/components/JobListings.vue'
+import { RouterView } from 'vue-router' 
 
-const title = ref('Become a Vue Dev')
 </script>
 
 <template>
   <Navbar />
-  <Hero :title="title"/>
-  <HomeCards />
-  <JobListings />
+  <RouterView />
 </template>
 
 
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Hero from '@/components/Hero.vue'
+import HomeCards from '@/components/HomeCards.vue'
+import JobListings from '@/components/JobListings.vue'
+import { ref } from 'vue'
+
+const title = ref('Become Vue Developer')
+</script>
+
+<template>
+  <Hero :title="title"/>
+  <HomeCards />
+  <JobListings />
+</template>
